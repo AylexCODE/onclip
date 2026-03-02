3e38b8e9f22d89d01d7e9ad940bf3ee05fec1290
Alex <130986014+AylexCODE@users.noreply.github.com>
2026-03-03 00:43:58 +0800

    Inital working clipboard

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:onclip/settings.dart';
+import 'package:onclip/utils/api_request.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,9 +12,44 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final TextEditingController clipboard = TextEditingController();
+
+  Future<String>? clipboardData;
+
+  String savedEndpoint = "";
+  String connectionStatus = "Connecting";
+  Color connectionIndicator = Colors.orangeAccent;
+
+  @override
+  void initState(){
+    super.initState();
+    getSavedEndpoint();
+  }
 
   void openDrawer() {
     scaffoldKey.currentState!.openEndDrawer();
+  }
+
+  Future<void> getSavedEndpoint() async{
+    final settings = await SharedPreferences.getInstance();
+
+    setState(() {
+      savedEndpoint = settings.getString('endpoint') ?? '';
+    });
+
+    getClipboardData(settings.getString("endpoint") ?? '', "Default");
+  }
+
+  void getClipboardData(String endpoint, String conn){
+    setState((){
+      connectionStatus = "Connecting";
+      connectionIndicator = Colors.orangeAccent;
+      clipboardData = getClipboard(endpoint, conn);
+    });
+  }
+
+  void setConnectionState(){
+    
   }
 
   @override
@@ -24,14 +61,14 @@ class _HomeState extends State<Home> {
           children: [
             InkWell(
               onTap: () {
-                print("refresh");
+                getClipboardData(savedEndpoint, "Default");
               },
               borderRadius: BorderRadius.circular(50),
               splashColor: Colors.yellowAccent,
               highlightColor: Colors.yellowAccent,
               child: CircleAvatar(
                 radius: 8,
-                backgroundColor: Colors.redAccent,
+                backgroundColor: connectionIndicator,
               ),
             ),
             Text("Default", 
@@ -39,7 +76,7 @@ class _HomeState extends State<Home> {
                 fontSize: 18
               ),
             ),
-            Text("Disconected", 
+            Text(connectionStatus, 
               style: TextStyle(
                 fontSize: 18
               ),
@@ -110,15 +147,61 @@ class _HomeState extends State<Home> {
       ),
       key: scaffoldKey,
       body: Center(
-        child: TextField(
-          maxLines: null,
-          expands: true,
-          textAlignVertical: TextAlignVertical.top,
-          keyboardType: TextInputType.multiline,
-          decoration: InputDecoration(
-            // hintText: 'Start Typing...',
-            border: InputBorder.none,
-          ),
+        child: FutureBuilder(
+          future: clipboardData,
+          builder: (context, snapshot){
+            if(snapshot.hasData){
+              connectionStatus = "Connected";
+              connectionIndicator = Colors.green;
+              clipboard.text = snapshot.data!;
+              return SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width, maxWidth: 5000),
+                  child: TextField(
+                    controller: clipboard,
+                    maxLines: null,
+                    expands: true,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Start Typing...',
+                      contentPadding: EdgeInsets.all(8),
+                    ),
+                    autocorrect: false,
+                    onChanged: (value) => {
+                      clipboard.text = value
+                    },
+                  ),
+                ),
+              );
+            }else if(snapshot.hasError){
+              connectionStatus = "Disconnected";
+              connectionIndicator = Colors.redAccent;
+            }
+            
+            return SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width, maxWidth: 5000),
+                  child: TextField(
+                    controller: clipboard,
+                    maxLines: null,
+                    expands: true,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Start Typing...',
+                      contentPadding: EdgeInsets.all(8),
+                    ),
+                    autocorrect: false,
+                    onChanged: (value) => {
+                      clipboard.text = value
+                    },
+                  ),
+                ),
+              );
+          },
         ),
       ),
       endDrawer: NavigationDrawer(
